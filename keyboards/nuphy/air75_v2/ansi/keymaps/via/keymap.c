112bd4b4dc01dc840177811ffc05251385da58f3
Stanley Zhang <stanley.zhang@ityin.net>
2026-07-26 15:11:01 +1200

    update to use different layout for home row modifiers

--- keyboards/nuphy/air75_v2/ansi/keymaps/via/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/via/keymap.c
@@ -67,20 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-		case LCTL_T(KC_A):
-		case LSFT_T(KC_S):
-		case LSFT_T(KC_L):
-		case RSFT_T(KC_L):
-        case LT(1, KC_D):
-        case LT(1, KC_K):
-        case LT(3, KC_D):
-        case LT(3, KC_K):
-		case LGUI_T(KC_F):
-		case LGUI_T(KC_J):
-		case RGUI_T(KC_J):
-		case LALT_T(KC_F):
-		case LALT_T(KC_J):
-		case RALT_T(KC_J):
+		case LSFT_T(KC_A):
+		case RSFT_T(KC_SCLN):
+        case LT(1, KC_S):
+        case LT(1, KC_L):
+        case LT(3, KC_S):
+        case LT(3, KC_L):
+		case LGUI_T(KC_D):
+		case RGUI_T(KC_K):
+		case LALT_T(KC_D):
+		case LALT_T(KC_K):
+		case LCTL_T(KC_F):
+		case LCTL_T(KC_J):
+		case RCTL_T(KC_J):
 		case LT(1, KC_SPC):
 		case LT(3, KC_SPC):
             // Do not select the hold action when another key is pressed.
